1db3a5711e8608b16ffe071916172633953ba58e
Juan Alien <oliveira.juan@ime.eb.br>
2025-04-15 21:35:24 -0300

    fix: main script

--- src/main.py
+++ src/main.py
@@ -5,16 +5,19 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 QUEUE_URL = os.getenv("QUEUE_URL")
 
-# Simulate sensor reading
-data = SensorData(heart_rate=140, network_type="3G")
-score = ScoreCalculator.calculate(data.heart_rate, data.network_type)
+def main():
+    # Simulate sensor reading
+    data = SensorData(heart_rate=140, network_type="3G")
+    score = ScoreCalculator.calculate(data.heart_rate, data.network_type)
 
-if score >= 0.9:
-    dispatcher = SQSDispatcher(QUEUE_URL)
-    payload = data.to_dict() | { "score": score }
-    dispatcher.send(payload)
-else:
-    print(f"ℹ️ Score {score} below threshold. Data ignored.")
+    if score >= 0.9:
+        dispatcher = SQSDispatcher(QUEUE_URL)
+        payload = data.to_dict() | { "score": score }
+        dispatcher.send(payload)
+    else:
+        print(f"ℹ️ Score {score} below threshold. Data ignored.")
+
+if __name__ == "__main__":
+    main()
